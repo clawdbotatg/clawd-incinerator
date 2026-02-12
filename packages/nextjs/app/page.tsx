acd5a379e002c49eb0d1485b611e52c7cb85396e
Austin Griffith <you@example.com>
2026-02-12 11:20:16 -0700

    Remove Last Burn section and event listening

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -2,7 +2,6 @@
 
 import { Address } from "@scaffold-ui/components";
 import { IncineratorPanel } from "./incinerator/_components/IncineratorPanel";
-import { RecentBurns } from "./incinerator/_components/RecentBurns";
 
 const INCINERATOR_ADDRESS = "0x536453350f2eee2eb8bfee1866baf4fca494a092";
 
@@ -19,7 +18,6 @@ export default function Home() {
       {/* Main content */}
       <div className="max-w-2xl mx-auto px-4 pb-16 space-y-8">
         <IncineratorPanel />
-        <RecentBurns />
 
         {/* Contract address */}
         <div className="flex justify-center">
